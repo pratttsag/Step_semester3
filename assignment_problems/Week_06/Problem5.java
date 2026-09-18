9a17877faa2038c55b4cb6e218d73c994300777c
pratttsag <pratsag11@gmail.com>
2026-09-18 19:37:28 +0530

    Add Week 6 assignment problems

---
+++ assignment_problems/Week_06/Problem5.java
@@ -0,0 +1,32 @@
+class Employee {
+
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class Problem5 {
+
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("Divya", 65000);
+        Employee employee2 = new Employee("Arjun", 50000);
+        Employee employee3 = new Employee("Rahul", 60000);
+
+        Employee.printCompanyInfo();
+    }
+}
